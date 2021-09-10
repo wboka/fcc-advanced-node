2ce436a58224c90214d7e8ab6eb3afaf3e7da83c
Glitch (wtb-fcc-advanced-node-2021) <none>
2021-09-10 15:01:07 +0000

    🍞🏪 finish advanced node 3

--- auth.js
+++ auth.js
@@ -23,10 +23,36 @@ module.exports = (app, myDataBase) => {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/auith/github/callback"
+        callbackURL: "https://wtb-fcc-advanced-node-2021.glitch.me/auth/github/callback"
       },
       (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
+
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              created_on: new Date(),
+              provider: profile.provider || ""
+            },
+            $set: {
+              last_login: new Date()
+            },
+            $inc: {
+              login_count: 1
+            }
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
       }
     )
   );
