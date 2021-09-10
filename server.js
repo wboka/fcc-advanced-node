f2fdc84d30fc1d12e680d48d9ad406b67a133480
Glitch (wtb-fcc-advanced-node-2021) <none>
2021-09-10 16:20:58 +0000

    ⛲️🍽 add auth to socket io

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const pug = require("pug");
 const mongodb = require("mongodb");
-const app = express();
+const session = require("express-session");
 
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const auth = require("./auth");
 const routes = require("./routes");
+
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo")(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -28,6 +35,17 @@ myDB(async client => {
   routes(app, myDataBase);
 
   let currentUsers = 0;
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
 
   io.on("connection", socket => {
     ++currentUsers;
@@ -48,6 +66,18 @@ myDB(async client => {
     res.render("pug", { title: e, message: "Unable to login" });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log("successful connection to socket.io");
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log("failed connection to socket.io:", message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
